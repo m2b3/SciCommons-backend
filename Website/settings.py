92d156f8d0e10928396242004b1a8cc3d9858c22
Jyothi Swaroop Reddy <bjyothiswaroop2002@gmail.com>
2023-10-28 18:36:16 +0530

    modified settings.py

--- Website/settings.py
+++ Website/settings.py
@@ -117,20 +117,20 @@ ASGI_APPLICATION = 'app.routing.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DBNAME'),
-#         'USER': config('DBUSER'),
-#         'PASSWORD': config('DBPASSWORD'),
-#         'HOST': config('DBHOST'),
-#         'PORT': config('DBPORT'),
-#     }
-# }
-
 DATABASES = {
-    'default':parse(config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DBNAME'),
+        'USER': config('DBUSER'),
+        'PASSWORD': config('DBPASSWORD'),
+        'HOST': config('DBHOST'),
+        'PORT': config('DBPORT'),
+    }
 }
+
+# DATABASES = {
+#     'default':parse(config('DATABASE_URL'))
+# }
 
 
 # Password validation
