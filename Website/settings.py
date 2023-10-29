ed28f113375096f568678a19cb7071ef096fc052
Jyothi Swaroop Reddy <bjyothiswaroop2002@gmail.com>
2023-10-30 01:28:29 +0530

    added settings.py

--- Website/settings.py
+++ Website/settings.py
@@ -122,7 +122,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'scicommons',
         'USER': 'postgres',
-        'PASSWORD': config('DBPASSWORD'),
+        'PASSWORD': 'Jyothiswaroop07',
         'HOST': 'scicommons.cgdneardo4hm.ca-central-1.rds.amazonaws.com',
         'PORT': 5432,
     }
