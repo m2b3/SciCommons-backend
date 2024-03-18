459a2dc8e0be4a74cf529c05f78bc73909cf4a7c
FIRSTNAME LASTNAME <rabugude@cisco.com>
2024-03-18 15:40:49 +0530

    removed merge conflicts, now code is same as original/main

--- app/tasks.py
+++
@@ -1,9 +0,0 @@
-# tasks.py
-
-from celery import shared_task
-from django.core.mail import send_mail
-from django.conf import settings
-
-@shared_task
-def send_email_task(subject, message, recipient_list):
-    send_mail(subject, message, settings.EMAIL_HOST_USER, recipient_list, fail_silently=False)
