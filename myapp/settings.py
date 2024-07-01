1ea4178d73a80f65b412ba867cf9e8832783d443
dinakar17 <85105044+dinakar17@users.noreply.github.com>
2024-07-01 23:01:52 +0530

    Allow all hosts

--- myapp/settings.py
+++ myapp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-$2jy%w9qfy0$x+dsm0gyb1jcy=tqe1(wc93fl9fg6^kazul8%h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
