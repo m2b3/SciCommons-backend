29434e507889a1e61861042000e72877becd6bce
dinakar17 <85105044+dinakar17@users.noreply.github.com>
2024-07-10 01:43:17 +0530

    provide defaults for db to run github actions

--- myapp/settings.py
+++ myapp/settings.py
@@ -121,11 +121,11 @@ WSGI_APPLICATION = "myapp.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USER"),
-        "PASSWORD": config("DB_PASSWORD"),
-        "HOST": config("DB_HOST"),
-        "PORT": config("DB_PORT"),
+        "NAME": config("DB_NAME", default="myapp"),
+        "USER": config("DB_USER", default="myapp"),
+        "PASSWORD": config("DB_PASSWORD", default="myapp"),
+        "HOST": config("DB_HOST", default="localhost"),
+        "PORT": config("DB_PORT", default="5432"),
     }
 }
 
