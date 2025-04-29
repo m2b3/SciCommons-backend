d9ac7b85188891065c83d73d16a0f1ca09529ecc
Mohd Faisal Ansari <fa058592@gmail.com>
2025-04-29 17:12:24 +0000

    changed the settings file

--- myapp/settings.py
+++ myapp/settings.py
@@ -68,6 +68,19 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": True,
 }
 
+MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_ratelimit.middleware.RatelimitMiddleware",
+    "ninja.compatibility.files.fix_request_files_middleware"
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
 
